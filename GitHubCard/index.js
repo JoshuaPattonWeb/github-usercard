e601abcf5dcf7af51cc5f674cd514364edeb7ae7
unknown <uxob03@outlook.com>
2021-10-29 02:03:23 -0400

    cannot find this error for the life of me please help monday!

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,6 +5,14 @@ import axios from 'axios'
     https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/JoshuaPattonWeb')
+.then(result => {
+  console.log(result.data)
+})
+.catch(error => {
+  console.error(error)
+})
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -31,6 +39,11 @@ import axios from 'axios'
 
 const followersArray = ['https://api.github.com/users/tetondan', 'https://api.github.com/users/dustinmyers', 'https://api.github.com/users/justsml', 'https://api.github.com/users/luishrd', 'https://api.github.com/users/bigknell'];
 
+followersArray.forEach(people => {
+  cardMaker(people)
+})
+
+cardMaker('JoshuaPattonWeb')
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -61,7 +74,7 @@ const followersArray = ['https://api.github.com/users/tetondan', 'https://api.gi
 */
 //function
     const card = document.querySelector('.cards')
-    function cardMaker({ avatar_url, name, login, location, html_url, followers, following, bio }) {
+    function cardMaker(info) {
 
 // elements
     const wholeCard = document.createElement('div')
@@ -90,38 +103,35 @@ const followersArray = ['https://api.github.com/users/tetondan', 'https://api.gi
 
 // element data
     wholeCard.classList.add('card')
-    content.classList.add('user-content')
+    content.classList.add('card-info')
     loginName.classList.add('name')
     userName.classList.add('username')
 
 // text addition
-    profileImg.src = avatar_url
-    loginName.textContent = name
-    userName.textContent = login
-    userLocation.textContent = `location: ${location}`
-    profile.textContent = 'Profile:'
-    profileLink.href = html_url
-    profileLink.textContent = html_url
-    userFollowers.textContent = `Followers: ${followers}`
-    userFollowing.textContent = `Following: ${following}`
-    userBio.textContent = `Bio: ${bio}`
-    console.log(wholeCard)
-    return cardMaker(wholeCard)
+  axios.get(`https://api.github.com/users/${info}`)
+    .then(result => {
+      profileImg.src = result.data.avatar_url
+      loginName.textContent = result.data.name
+      userName.textContent = 'Username: ' + result.data.login
+      userLocation.textContent = 'Location: ' + result.data.location
+      profile.textContent = 'Profile:'
+      profileLink.href = result.data.html_url
+      profileLink.textContent = result.data.html_url
+      userFollowers.textContent = 'Followers: ' + result.data.followers
+      userFollowing.textContent = 'Following: ' + result.data.following
+      userBio.textContent = 'Bio: ' + result.data.bio
+    })
+    .catch(error => {
+      console.error(error)
+    })
+    const pullCard = card.appendChild(wholeCard)
+    return pullCard
 }
 
-const test = axios.get('https://api.github.com/users/JoshuaPattonWeb')
-.then(result => {
-  const data = result.data
+
   // const cards = document.querySelector('.cards')
-  const gitCard = cardMaker(result.data) 
-  cards.appendChild(gitCard)
   // follow step 4
   // get the element with a class of cards from teh DOM, document.querySelector('cards')
   // append the user card to the element we just querySelected
-})
-.catch(error => {
-  console.error(error)
-})
-
 //receiving error and i looked over it numerous times and I cannot seem to find the issue https://prnt.sc/1xoo8kc
 // its really stressing me out and its not portraying any code. Its not showing where error is located.
